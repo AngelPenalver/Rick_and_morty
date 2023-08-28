52200fd8b6891ddecc05fbee5d4974b802ff76f1
AngelPenalver <apenalver4@gmail.com>
2023-08-28 14:55:00 -0400

    actualizacion rick_and_morty

---
+++ src/redux/Reducer/index.js
@@ -0,0 +1,19 @@
+import { ADD_FAV, REMOVE_FAV } from "../Action";
+let initialState = { myFavorites: [] };
+export default function rootReducer(state = initialState, action) {
+    switch (action.type) {
+        case ADD_FAV:
+            return {
+                ...state,
+                myFavorites: [...state.myFavorites, ...action.payload]
+            }
+        case REMOVE_FAV:
+            return {
+                ...state,
+                myFavorites: state.myFavorites.filter(character => character.id !== parseInt(action.payload))
+
+            }
+        default:
+            return state;
+    }
+}
